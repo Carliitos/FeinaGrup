2736411554cc6ac8d7962544e8c62727456db3a8
Adrian <Adrian@DESKTOP-6A0CMNU>
2018-10-03 18:30:19 +0200

    Crea funcio carlos

--- adrian.php
+++ adrian.php
@@ -10,7 +10,12 @@ and open the template in the editor.
         <title></title>
     </head>
     <body>
-        <?php            
+        <?php
+        class AdrianClass{
+        public function Carlos() {
+            return "";
+        }
+        }
         ?>
     </body>
 </html>
