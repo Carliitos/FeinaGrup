6c685327940e5d205e99bdb19a4f05d14bf11783
Andres <Andres@DESKTOP-PJHMP6I>
2018-10-03 20:23:29 +0200

    Tercera funció

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        class Andres {
+            function Andres()
+            {
+              echo "Andres";  
+            }      
+                             
+            function Carlos()
+            {
+                echo "Carlos";
+            }
+            function Adrian()
+            {
+                echo "Adrian";
+            }
+         };
+      
+        ?>
+    </body>
+</html>
