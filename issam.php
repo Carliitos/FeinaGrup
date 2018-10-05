f35c1de813599548ab29b9a872a20fc949224659
issamm6 <issam.996@hotmail.com>
2018-10-05 16:15:25 +0200

    CommitCodiIssam

---
+++ issam.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        class IssamClass{
+        public function Carlos() {
+            echo "Carlos";
+        }
+        public function Andres(){
+            echo "Andres";
+        }
+        public function Adrian(){
+            echo "Adrian";
+        }
+        public function Issam(){
+            echo "Issam";
+        }
+        }
+        ?>
+    </body>
+</html>
